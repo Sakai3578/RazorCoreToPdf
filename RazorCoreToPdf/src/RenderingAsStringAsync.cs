5bb112a90d3fc3188ac30038aa099b81b43f2f5c
DESKTOP-H7NAULB\sakai <sakai3578@gmail.com>
2022-11-14 16:23:09 +0900

    プロジェクト ファイルを追加します。

---
+++ RazorCoreToPdf/src/RenderingAsStringAsync.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace RazorCoreToPdf {
+    internal static class RenderingAsStringAsync {
+        internal static async Task<string> Run(
+            string viewName,
+            object model,
+            ControllerContext controllerContext,
+            bool isPartial = false
+        ) {
+
+            var actionContext = controllerContext as ActionContext;
+            var razorViewEngine = controllerContext.HttpContext.RequestServices.GetService(typeof(IRazorViewEngine)) as IRazorViewEngine;
+            var tempDataProvider = controllerContext.HttpContext.RequestServices.GetService(typeof(ITempDataProvider)) as ITempDataProvider;
+
+
+
+
+
+            using var stringWriter = new StringWriter();
+            var viewResult = razorViewEngine!.FindView(actionContext, viewName, !isPartial);
+
+            if (viewResult?.View == null) {
+                throw new ArgumentException($"Fatal: {viewName} does not match any available view.");
+            }
+
+            var viewDictionary = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { Model = model };
+
+            var viewContext = new ViewContext(
+                actionContext,
+                viewResult.View,
+                viewDictionary,
+                new TempDataDictionary(actionContext.HttpContext, tempDataProvider!),
+                stringWriter,
+                new HtmlHelperOptions()
+            );
+
+            await viewResult.View.RenderAsync(viewContext);
+            return stringWriter.ToString();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorCoreToPdf;
+
+namespace WebApplication1.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index() {
+            return View();
+        }
+        public async Task<IActionResult> Ass() {
+            return View();
+        }
+        public async Task<IActionResult> Opp() {
+            return await this.Download("Index", DateTime.Now);
+        }
+    }
+}
